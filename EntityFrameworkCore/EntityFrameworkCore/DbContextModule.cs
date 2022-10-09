d13693845abf4f08460d7b5fd3cb16ccdb224492
Adam Meyer <ameyer15801@gmail.com>
2022-10-09 12:54:38 -0500

    DbContextModule

---
+++ EntityFrameworkCore/EntityFrameworkCore/DbContextModule.cs
@@ -0,0 +1,25 @@
+﻿using Meyer.Common.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Boosted.Entities;
+
+public static class DbContextModule
+{
+    public static IServiceCollection AddDbConnectionOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services.AddScoped(x => configuration.GetSection(DbConnectionOptions.SectionName).Get<DbConnectionOptions>());
+    }
+
+    public static IServiceCollection AddMultiTenantDbContextFactory<T>(this IServiceCollection services) where T : DbContextBase
+    {
+        return services
+            .AddScoped<IMultiTenantDbContextFactory<T>, MultiTenantDbContextFactory<T>>()
+            .AddDbContextFactory<T>(x => { }, ServiceLifetime.Scoped);
+    }
+
+    public static IServiceCollection AddSingleTenantDbContextPool<T>(this IServiceCollection services) where T : DbContextBase
+    {
+        return services.AddDbContextPool<T>(x => { });
+    }
+}
